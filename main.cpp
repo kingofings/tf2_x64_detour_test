308196a5a46b02dde9bfeecc0ba51f0461f24002
kingofings <47538464+kingofings@users.noreply.github.com>
2024-03-19 00:12:18 +0100

    Mad milk patch (unstable with detour)

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 #include <unistd.h>
 #include <cstring>
 #include <funchook.h>
-
+#include <iomanip>
+#include <sys/mman.h>
+#include <chrono>
+#include <thread>
 
 #define CTFPlayer_OnTakeDamage_ADDRESS 0x00000000011e5310
+#define MAD_MILK_MULT_ADDRESS 0x0000000000B947D8
+#define MAD_MILK_MOVSS_FLOAT_OPERAND_OFFSET 0x009062CC
 #define CTakeDamageInfo_m_eCritType_OFFSET 100
 #define CTakeDamageInfo_m_bitsDamageType_OFFSET 60
 #define DMG_CRITICAL 1048576
@@ -21,6 +26,8 @@ using CTFPlayer_OnTakeDamage = int (*)(void* pThis, void* pDamageInfo);
 
 CTFPlayer_OnTakeDamage originalOnTakeDamage = nullptr;
 
+ulong BaseAddress = 0x0;
+
 int Detour_OnTakeDamage(void* pThis, void* pDamageInfo)
 {
     std::cout << "CTFPlayer::OnTakeDamage detour called!" << std::endl;
@@ -34,7 +41,7 @@ int Detour_OnTakeDamage(void* pThis, void* pDamageInfo)
         return originalOnTakeDamage(pThis, pDamageInfo);
     }
 
-    //Only set if it's not already Critical it breaks for some reason and i don't want to look into it
+    //Only set if it's not already Critical it breaks for some reason and I don't want to look into it
     if (*pCritType != CritType_Crit && !(*pDamageType & DMG_CRITICAL))
     {
         *pCritType = CritType_Crit;
@@ -60,33 +67,29 @@ void GetFunctionPointer()
     }
 
     char line[256];
-    unsigned long baseAddress = 0;
 
     while (fgets(line, sizeof(line), pFile))
     {
         if (strstr(line, "server_srv.so") != NULL && strstr(line, "r--p") != NULL)
         {
-            baseAddress = strtoul(line, NULL, 16);
+            BaseAddress = strtoul(line, NULL, 16);
             break;
         }
     }
 
     fclose(pFile);
 
-    *(void**)(&originalOnTakeDamage) = (void*)(baseAddress + CTFPlayer_OnTakeDamage_ADDRESS);
+    *(void**)(&originalOnTakeDamage) = (void*)(BaseAddress + CTFPlayer_OnTakeDamage_ADDRESS);
 
     std::cout << "CTFPlayer::OnTakeDamage Address: " << &originalOnTakeDamage << std::endl;
 }
 
-__attribute__((constructor))
-void setupDetour()
+void SetupDetour()
 {
-    GetFunctionPointer();
-
     std::cout << "Trying to create funchook" << std::endl;
     funchook_t* pFunchook = funchook_create();
 
-    int result = funchook_prepare(pFunchook, (void**)&originalOnTakeDamage, (int*)Detour_OnTakeDamage);
+    int result = funchook_prepare(pFunchook, (void**)&originalOnTakeDamage, (void*)Detour_OnTakeDamage);
 
     if (result != FUNCHOOK_ERROR_SUCCESS)
     {
@@ -103,4 +106,82 @@ void setupDetour()
     }
 
     std::cout << "Prepared Detour!" << std::endl;
+}
+
+
+void SetupPatchMadMilkMult()
+{
+    std::cout << "Patching Mad Milk Multiplier..." << std::endl;
+
+    unsigned char* pBytes = reinterpret_cast<unsigned char*>(BaseAddress + MAD_MILK_MULT_ADDRESS);
+
+    const unsigned char signature[] = {0xF3, 0x0F, 0x10, 0x05};
+
+    const size_t signatureLength = sizeof(signature) / sizeof(signature[0]);
+
+    bool match = std::memcmp(pBytes, signature, signatureLength) == 0;
+
+    for (size_t i = 0; i < signatureLength; i++)
+    {
+        std::cout << "Byte " << i << ": "
+        << std::hex << std::setw(2) << std::setfill('0')
+        << static_cast<int>(pBytes[i]) << std::endl;
+    }
+
+    if (!match)
+    {
+        std::cerr << "Byte Signature does not match for Mad Milk Patch!" << std::endl;
+        return;
+    }
+
+    //the movss instruction is 8 bytes long
+    uintptr_t targetaddress = reinterpret_cast<uintptr_t>(pBytes) + 8 + MAD_MILK_MOVSS_FLOAT_OPERAND_OFFSET;
+
+    float* pValue = reinterpret_cast<float*>(targetaddress);
+
+    std::cout << "Default value: " << *pValue << std::endl << "Allocating new memory..." << std::endl;
+
+    //Yes in this example we will have this dangling around and leaking. We do not care in this scenario
+    float* pFloat = reinterpret_cast<float*>(malloc(sizeof(float)));
+
+    *pFloat = 0.1f;
+
+    std::cout << "Writing Bytes..." << std::endl;
+
+    uintptr_t nextInstructionAddress = reinterpret_cast<uintptr_t>(pBytes + 8);
+    int32_t newOffSet = reinterpret_cast<uintptr_t>(pFloat) - nextInstructionAddress;
+
+    size_t pageSize = getpagesize();
+    uintptr_t pageStart = reinterpret_cast<uintptr_t>(pBytes) & -pageSize;
+
+    /*
+     * This here is unstable AF if this patch is applied after players have joined any execution of
+     * CTFWeaponBase::ApplyOnHitAttributes while someone has the milk condition will crash.
+     * I don't know why yet because I probably don't understand something yet.
+     * Will need to research
+     */
+
+    /*
+     * UPDATE: I now apply the detour after the patch now it's reversed it will crash if you apply it before the
+     * player joining
+     */
+    if (mprotect(reinterpret_cast<void*>(pageStart), pageSize, PROT_READ | PROT_WRITE | PROT_EXEC) != 0)
+    {
+        perror("mprotect");
+        return;
+    }
+
+    std::memcpy(pBytes + 4, &newOffSet, sizeof(newOffSet));
+
+    std::cout << "Wrote new allocated memory with new Value to operand: " << *pFloat << std::endl;
+
+}
+
+__attribute__((constructor))
+void Setup()
+{
+    GetFunctionPointer();
+    SetupPatchMadMilkMult();
+    //some weird stuff happens when the detour is active to the madmilk patch have to figure this out
+    //SetupDetour();
 }
